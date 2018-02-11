d34fe5ee2e5041acf786b17d2a4ca9923c37ded9
Junior Miqueletti <junior.miqueletti@matera.com>
2018-02-11 09:53:20 -0200

    adding first test

---
+++ src/test/test/com/juniormiqueletti/camel/request/RouteRequestTest.java
@@ -0,0 +1,34 @@
+package com.juniormiqueletti.camel.request;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RouteRequestTest {
+
+	private CamelContext context;
+	
+	@Before
+	public void setUp() {
+		context = new DefaultCamelContext();
+	}
+	
+	@Test
+	public void basicTest() throws Exception {
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:requests?noop=true")
+				.log("${exchange.pattern}")
+				.log("${id} - ${body}")
+				.to("file:out");
+			}
+		});
+		
+		context.start();
+		Thread.sleep(5000);
+	}
+}
