d786cd8619676f13f7f8e6e3daf65c70c4a85003
Junior Miqueletti <junior.miqueletti@matera.com>
2018-02-13 16:11:17 -0200

    adding Http to File test

---
+++ src/test/test/com/juniormiqueletti/camel/request/HttpToFileTransferTest.java
@@ -0,0 +1,56 @@
+package com.juniormiqueletti.camel.request;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class HttpToFileTransferTest {
+
+	private CamelContext context;
+	
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+	
+	@Before
+	public void setUp() {
+		context = new DefaultCamelContext();
+	}
+	
+	@Test
+	public void basicTest() throws Exception {
+		String absolutePath = tempFolder.getRoot().getAbsolutePath();
+		String uriOut = "file:" + absolutePath;
+//		String uriOut = "file:out";
+		
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				
+				from("timer://negociacoes?fixedRate=true&delay=1s&period=360s")
+				.to("http4://argentumws.caelum.com.br/negociacoes")
+					.convertBodyTo(String.class)
+					.log("${body}")
+					.setHeader(Exchange.FILE_NAME, constant("negotiations.xml"))
+				.to(uriOut);
+			}
+		});
+		
+		context.start();
+		Thread.sleep(2000);
+		
+		long numberFiles = Arrays.asList(tempFolder.getRoot().listFiles())
+			.stream()
+			.count();
+		
+		assertTrue(numberFiles > 0);
+	}
+}
